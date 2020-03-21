dfe8fe8f0e97cb7df14369742ef7e9ccbb808f0f
Shavlovsii_Ivan <na613c613c@gmail.com>
2020-03-21 13:46:56 +0300

    [UPD] preservation before using redux

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import * as serviceWorker from './serviceWorker';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
-import store from './redux/state';
+import store from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
 
 let rerenderEntireTree = (state) => {
