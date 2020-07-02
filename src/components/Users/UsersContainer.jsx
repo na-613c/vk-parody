fa3a7e7c723274a20efce9b4466f1d016f6ab54b
Shavlovsii_Ivan <na613c613c@gmail.com>
2020-07-03 00:33:22 +0300

    [ADD] pagination

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -2,24 +2,33 @@
 import React from "react";
 import Users from "./Users";
 import {connect} from "react-redux";
-import {followAC, setUsersAC, unfollowAC} from "../../redux/users-reducer";
+import {followAC, setCurrentPageAC, setUsersAC, setUsersTotalCountAC, unfollowAC} from "../../redux/users-reducer";
 
 let mapStateProps = (state) => {
     return {
-        users: state.usersPage.users
+        users: state.usersPage.users,
+        pageSize: state.usersPage.pageSize,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        currentPage: state.usersPage.currentPage
     }
 };
 
 let mapDispatchToProps = (dispatch) => {
     return {
-        follow : (userId) => {
+        follow: (userId) => {
             dispatch(followAC(userId))
         },
-        unfollow : (userId) => {
+        unfollow: (userId) => {
             dispatch(unfollowAC(userId))
         },
-        setUsers : (users) => {
+        setUsers: (users) => {
             dispatch(setUsersAC(users))
+        },
+        setCurrentPage: (currentPage) => {
+            dispatch(setCurrentPageAC(currentPage))
+        },
+        setTotalUsersCount: (totalCount) => {
+            dispatch(setUsersTotalCountAC(totalCount))
         }
     }
 };
