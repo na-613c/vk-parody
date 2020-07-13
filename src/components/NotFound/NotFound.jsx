ab23c598507c4f0b1e60d00c2817ec374ec2744e
Shavlovsii_Ivan <na613c613c@gmail.com>
2020-07-14 01:23:32 +0300

    [UDP] Buttons and input

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import s from "./NotFound.module.css"
-import notFoundImg  from "../../assets/images/not_found.gif"
+import notFoundImg from "../../assets/images/not_found.gif"
 
-const NotFound = () =>{
-    return(
+const NotFound = () => {
+    return (
         <div className={"shadow " + s.notFound}>
-            <h1>Страница не существует</h1>
+            <h1>Страница <br/>
+                не существует </h1>
             <img src={notFoundImg} alt="404"/>
         </div>
     )
